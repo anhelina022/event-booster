b9221e4b9f583c8a77a2c4cb99466023ee55fcd8
Ангеліна Бебешко <angelinabebesko05@gmail.com>
2026-07-10 12:54:02 +0100

    add commit

--- event-bootster/src/main.js
+++ event-bootster/src/main.js
@@ -3,27 +3,27 @@ const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 
 const eventsContainer = document.querySelector(".events");
 
+let events = [];
+
 async function getEvents() {
-    try {
+  try {
     const response = await fetch(`${BASE_URL}?apikey=${API_KEY}`);
     const data = await response.json();
 
-    console.log(data);
-
-    const events = data._embedded.events;
+    events = data._embedded.events;
     renderEvents(events);
-    } catch (error) {
+  } catch (error) {
     console.log(error);
-    }
+  }
 }
 
 function renderEvents(events) {
-    eventsContainer.innerHTML = "";
+  eventsContainer.innerHTML = "";
 
-const markup = events
-    .map(event => {
-        return `
-        <div class="event-card">
+  const markup = events
+    .map((event) => {
+      return `
+        <div class="event-card" data-id="${event.id}">
             <img class="event-card__img" src="${event.images[0].url}" alt="${event.name}">
             <h3 class="event-card__title">${event.name}</h3>
             <p class="event-card__date">${event.dates.start.localDate}</p>
@@ -33,7 +33,50 @@ const markup = events
     })
     .join("");
 
-    eventsContainer.innerHTML = markup;
+  eventsContainer.innerHTML = markup;
 }
 
 getEvents();
+
+const backdrop = document.querySelector(".backdrop");
+const modalContent = document.querySelector(".modal__content");
+const closeBtn = document.querySelector(".modal__close");
+
+eventsContainer.addEventListener("click", (event) => {
+    const card = event.target.closest(".event-card");
+
+if (!card) return;
+
+const eventId = card.dataset.id;
+const currentEvent = events.find((item) => item.id === eventId);
+
+modalContent.innerHTML = `
+    <div class="modal__div-logo">
+    <img src="${currentEvent.images[0].url}" alt="${currentEvent.name}" class="modal__image-logo">
+    </div>
+
+    <div class="modal__div-img">
+    <img src="${currentEvent.images[0].url}" alt="${currentEvent.name}" class="modal__image">
+    </div>
+
+    <div class="modal__div">
+    <h2>${currentEvent.name}</h2>
+    <p>${currentEvent.info || "No description"}</p>
+    </div>
+
+    <div class="modal__div">
+    <p>${currentEvent.dates.start.localDate}</p>
+    <p>${currentEvent._embedded.venues[0].name}</p>
+    </div>
+    
+    class="modal__div-btn">
+    <button class="modal__btn">MORE FROM THIS AUTHOR</button>
+    </div>
+    `;
+
+    backdrop.classList.remove("is-hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+    backdrop.classList.add("is-hidden");
+});
